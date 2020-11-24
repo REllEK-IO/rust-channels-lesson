103d0de828409478bad60a1dafa3ebbc4c1e9bca
mtKeller <micah.t.keller@gmail.com>
2020-11-23 16:44:48 -0800

    Fleshed out and added ping pong test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-use std::sync::{Arc, Condvar, Mutex};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Condvar, Mutex}
+};
 
 //Arc - A thread-safe reference counting pointer. 'Arc' stands for 'Atomically Reference Counted'
 // -- Used across thread boundaries
@@ -18,17 +13,51 @@ pub struct Sender<T> {
     inner: Arc<Inner<T>>,
 }
 
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        Sender { 
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+impl<T> Sender<T> {
+    pub fn send(&mut self, t: T) {
+        let mut queue = self.inner.queue.lock().unwrap();
+        queue.push_back(t);
+        drop(queue);
+        self.inner.available.notify_one();
+    }
+}
+
 pub struct Receiver<T> {
     inner: Arc<Inner<T>>,
 }
 
-struct Inner<T> {
-    queue: Mutex<Vec<T>>,
+
+impl<T> Receiver<T> {
+    pub fn recv(&mut self) -> T {
+        loop {
+            let mut queue = self.inner.queue.lock().unwrap();
+            match queue.pop_front() {
+                Some(t) => return t,
+                None => {
+                    queue = self.inner.available.wait(queue).unwrap();
+                }
+            }
+        }
+    }
 }
 
-pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+struct Inner<T> {
+    queue: Mutex<VecDeque<T>>,
+    available: Condvar,
+}
+
+pub fn _channel<T>() -> (Sender<T>, Receiver<T>) {
     let inner = Inner {
         queue: Mutex::default(),
+        available: Condvar::new(),
     };
     let inner = Arc::new(inner);
     (
@@ -39,4 +68,15 @@ pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
             inner: inner.clone(),
         }
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_ping_pong() {
+        let (mut tx, mut rx) = _channel();
+        tx.send(42);
+        assert_eq!(rx.recv(), 42);
+    }
 }
