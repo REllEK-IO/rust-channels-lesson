561a2cf797c12f3882d8b2b13591d6011fe0e4a7
mtKeller <micah.t.keller@gmail.com>
2020-11-23 16:14:34 -0800

    Notes

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,38 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+use std::sync::{Arc, Condvar, Mutex};
+
+//Arc - A thread-safe reference counting pointer. 'Arc' stands for 'Atomically Reference Counted'
+// -- Used across thread boundaries
+// -- Ensures that Sender and Receiver in this context are linked
+//Mutex - A mutual exclusive primitive useful for protecting shared data
+// -- Mutex lock returns guard which stops rewrite of value until released
+
+pub struct Sender<T> {
+    inner: Arc<Inner<T>>,
+}
+
+pub struct Receiver<T> {
+    inner: Arc<Inner<T>>,
+}
+
+struct Inner<T> {
+    queue: Mutex<Vec<T>>,
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let inner = Inner {
+        queue: Mutex::default(),
+    };
+    let inner = Arc::new(inner);
+    (
+        Sender {
+            inner: inner.clone(),
+        },
+        Receiver {
+            inner: inner.clone(),
+        }
+    )
+}
